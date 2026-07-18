c95337fa69bbd9bb46e8cb4fe49e904cff621059
zdwnss1 <125186277+zdwnss1@users.noreply.github.com>
2026-07-19 04:13:44 +0800

    Export calculus runtime manager

--- packages/world-runtime/src/index.ts
+++ packages/world-runtime/src/index.ts
@@ -2,4 +2,5 @@ export * from './types.js';
 export * from './scheduler.js';
 export * from './journal.js';
 export * from './windows.js';
+export * from './corePrograms.js';
 export * from './runtime.js';
