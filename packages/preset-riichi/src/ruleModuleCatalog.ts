4b3da3f0f3f054ac1754e792d50009ca8f718d47
zdwnss1 <125186277+zdwnss1@users.noreply.github.com>
2026-07-19 18:29:54 +0800

    Catalog declarative Turbo modules

--- packages/preset-riichi/src/ruleModuleCatalog.ts
+++ packages/preset-riichi/src/ruleModuleCatalog.ts
@@ -1,8 +1,10 @@
 import type { RuleModuleDefinition } from '@mahjongplus/world-language';
 import { LOCAL_YAKU_MODULES } from './localYaku.js';
 import { SUPER_RIICHI_MODULE } from './superRiichiModule.js';
+import { TURBO_RIICHI_MODULES } from './turboRiichiModules.js';
 
 export const RIICHI_RULE_MODULES: RuleModuleDefinition[] = [
   ...LOCAL_YAKU_MODULES,
   SUPER_RIICHI_MODULE,
+  ...TURBO_RIICHI_MODULES,
 ];
